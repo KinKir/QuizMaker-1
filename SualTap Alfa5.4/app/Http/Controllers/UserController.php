8967b3f52435f46a34455ce750c8eedacb97ad78
Murad Sofiyev <murad.s@code.edu.az>
2016-05-06 18:57:50 -0700

    Update UserAuth

--- SualTap Alfa5.4/app/Http/Controllers/UserController.php
+++ SualTap Alfa5.4/app/Http/Controllers/UserController.php
@@ -8,6 +8,11 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function quizresult()
     {
     	return view('user.quizresult');
